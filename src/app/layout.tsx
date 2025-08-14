6ad148a52baeb55dd3a18da2169d4ba42abce9a6
Sreya Satheesh <sreya.satheesh009@gmail.com>
2025-08-14 13:37:51 +0530

    fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode; 
 }>) {
   return (
     <html lang="en" className="dark">
